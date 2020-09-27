53f29281bca05297579eb625ff0305349a37fead
gossoudarev <goss@itmo.ru>
2020-09-27 23:13:23 +0300

    d

--- goss92xblock/goss92xblock.py
+++ goss92xblock/goss92xblock.py
@@ -113,7 +113,7 @@ class Goss92XBlock(ScorableXBlockMixin, XBlock):
         """).format(CURRENT)
         frag.add_content(SafeText(res0))
 
-        HTMLURL = 'https://node-server.online/r/assets/x92c.html'
+        HTMLURL = 'https://node-server.online/r/assets/x92d.html'
         if sys.version_info.major >= 3:
             response = urlopen(HTMLURL)
             encoding = response.info().get_content_charset('utf-8')
