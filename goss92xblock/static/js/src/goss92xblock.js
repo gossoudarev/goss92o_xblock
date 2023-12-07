b3614bba0846be2a5827cd1e536493213c6c1914
Ilya Goss <goss@itmo.ru>
2023-12-08 02:20:43 +0300

    Update goss92xblock.js v2023

--- goss92xblock/static/js/src/goss92xblock.js
+++ goss92xblock/static/js/src/goss92xblock.js
@@ -1,17 +1,22 @@
 /* Javascript for goss8XBlock. */
-function Goss92XBlock(runtime, element) {
+function Goss92XBlock(runtime, element, context) {
 
     window.updateScore = function (result) {
         $('.score', element).text(result.score);
+        localStorage['score'] = result.score;
+        localStorage['score_' + Math.random()] = result.score;
+        localUpdate(result.score);
     }
 
     window.handlerUrl = runtime.handlerUrl(element, 'set_score2');
 
+    init_goss(context); // important! here we initiate the week number check
+
     $('#gosssend').click(buttonClick);
 
-
     $(function ($) {
-        console.log('*x92j***********');
+        console.log('*x92***********');
         /* Here's where you'd do things on page load. */
     });
 }
+
